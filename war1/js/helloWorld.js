9e68fbff116875ea3eacf512e061e2d5caf7ee88
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-11-21 20:42:46 +0500

    completed js hello challenge

---
+++ war1/js/helloWorld.js
@@ -0,0 +1,7 @@
+//use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+//refer to the example and write your first JS function
+function helloWorld(){
+    let hello = "Hello World!"
+    console.log(hello)
+  }

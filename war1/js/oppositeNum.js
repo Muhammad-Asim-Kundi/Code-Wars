5e2b2ec92e307eaf950e12cec103650729f1b24c
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-12-09 18:22:50 +0500

    Finding the opposite of floating-point or integer

---
+++ war1/js/oppositeNum.js
@@ -0,0 +1,26 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+
+  return(-number);
+
+}
+
+console.log(opposite(11.88))
+
+
+//another old way
+
+// function opposite(number) {
+//   if(number == -number){
+//     return -1*number
+//   }else{
+//     return number*-1
+//   }
+// }

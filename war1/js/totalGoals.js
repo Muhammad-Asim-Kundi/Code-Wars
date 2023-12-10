0a2e8f3c16299b6a40a794b436ea647f3369fbf0
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-12-10 13:59:21 +0500

    Messi total goal function

---
+++ war1/js/totalGoals.js
@@ -0,0 +1,5 @@
+//return the total number of goals by messi in three leages of soccor
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals+copaDelReyGoals+championsLeagueGoals
+  }

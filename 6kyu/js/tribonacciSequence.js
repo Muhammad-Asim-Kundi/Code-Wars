de9f978131c320063488f7678624000b6a2045ab
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-12-19 23:33:02 +0500

    Tribonacci Sequence

---
+++ 6kyu/js/tribonacciSequence.js
@@ -0,0 +1,42 @@
+// Well met with Fibonacci bigger brother, AKA Tribonacci.
+
+// As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+
+// So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+// [1, 1 ,1, 3, 5, 9, 17, 31, ...]
+// But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+// [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+// Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+
+// Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+/* code */
+
+function tribonacci(signature, n) {
+    // Check for special case when n is 0
+    if (n === 0) {
+      return [];
+    } else if (n <= signature.length) {
+        // Return the first n elements if n is less than or equal to the length of the signature
+      return signature.slice(0, n);
+
+      //ANOTHER WAY TO WRITE THIS CODE
+      
+      // let newNum = 0
+      // for(let i=0; i<array.length; i++){
+      //         newNum += array[i]
+      //   }
+      //   return newNum
+    } else {
+      // Generate the Tribonacci sequence
+      for (let i = signature.length; i < n; i++) {
+        let sum = signature.slice(-3).reduce((acc, c) => acc + c, 0);
+        signature.push(sum);
+      }
+      return signature;
+    }
+  }
+  
+  console.log(tribonacci([1, 1, 1], 10));

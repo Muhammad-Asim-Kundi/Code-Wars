e70f838c913c9e60b01afe62db43d5c3786801d0
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-12-20 11:17:08 +0500

    Convert number to String

---
+++ war1/js/numToString.js
@@ -0,0 +1,21 @@
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+/* code */
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString()
+  }
+  
+  numberToString(56)
+
+/* another code example */
+
+const numberToString = num => num.toString();

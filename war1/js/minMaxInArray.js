821697c4ef4002d29a9c1e48b1a04ad79734df7c
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2023-12-27 23:56:42 +0500

    returning the min and max value in array

---
+++ war1/js/minMaxInArray.js
@@ -0,0 +1,15 @@
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// Examples (Input --> Output)
+// [1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+
+/** code  */
+
+function minMax(arr){
+    //returning both max and min value in array
+    return [Math.min(...arr),Math.max(...arr)]
+    
+  }

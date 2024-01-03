55d8d91b13097cdeaaa8205567ba08d3059c3771
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-01-03 22:19:04 +0500

    converting input to binary

---
+++ 6kyu/js/fakeBinary.js
@@ -0,0 +1,21 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+/** code  */
+
+function fakeBin(x){
+    //converting all x elements to string to loop through
+   let f = x.toString()
+   //creating n to store converted values
+    let n = ''
+    //looping through f to get new converted value to binary
+    for(let i=0; i<f.length; i++){
+      if(f[i]<5){
+        n += '0'
+      }else{
+        n += '1'
+      }
+    }
+    return n
+  }
--- 6kyu/js/populationGrowth.js
+++ 6kyu/js/populationGrowth.js
@@ -1,4 +1,7 @@
-// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater than or equal to p = 1200 inhabitants?
+// In a small town the population is p0 = 1000 at the beginning of a year. 
+//The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to
+// live in the town. How many years does the town need to see its population greater than or equal to
+// p = 1200 inhabitants?
 
 // At the end of the first year there will be: 
 // 1000 + 1000 * 0.02 + 50 => 1070 inhabitants

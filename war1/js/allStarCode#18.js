bec2309f862bdf3033e36c0a008172493a9c04e3
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-01-23 22:20:04 +0500

    return the number of string equal to given parameter

---
+++ war1/js/allStarCode#18.js
@@ -0,0 +1,34 @@
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// str_count("Hello", 'o'); // returns 1
+// str_count("Hello", 'l'); // returns 2
+// str_count("", 'z'); // returns 0
+// Notes
+// The first argument can be an empty string
+// In languages with no distinct character data type, the second argument will be a string of length 1
+
+/** Code */
+
+function strCount(str, letter){  
+    //code here
+    let num = 0
+    for(let i=0; i<str.length; i++){
+      if(str[i]==letter){
+       num+=1
+      }else{
+      num+=0
+      }
+    }
+    return num
+}
+
+/** Another Code */
+
+function strCounts(str, letter){  
+    return str.split('').filter(c => c == letter).length;
+  }

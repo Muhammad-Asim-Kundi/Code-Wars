3a66b9b03e12f8fac021f1e02a8631755457c22d
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-02-09 15:25:15 +0500

    return the sum of the odds in rows

--- 6kyu/js/nthRowSum.js
+++ 6kyu/js/nthRowSum.js
@@ -27,3 +27,24 @@ function rowSumOddNumbers(n) {
     }
     return sum 
 }
+
+
+
+//practice//
+
+function rowOddSum(n){
+
+  //obtaining the first number of each row
+  let first = n * (n-1) + 1
+
+  // sum variable to store the sum of all values
+  let sum = 0
+  // using for loop to iterate till the nth row and calculate sum
+   for (let i = 0; i< n; i++){
+    //adding each number to sum variable
+    sum += first + 2 * i
+   }
+   // returning the sum the final value
+   return sum 
+}
+

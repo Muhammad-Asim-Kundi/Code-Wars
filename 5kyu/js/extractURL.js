8457282d2edae9673606285500ccc1bba868b527
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-02-19 19:13:23 +0500

    Getting the Domain-Name from the url

--- 5kyu/js/extractURL.js
+++ 5kyu/js/extractURL.js
@@ -25,3 +25,13 @@ function extractDomainName(url){
     url = url.replace("www.", '');
     return url.split('.')[0];
   };
+
+
+/** Practice  */
+
+function GetDomainName(url){
+    url = url.replace("https://", '')
+    url = url.replace("http://", '')
+    url = url.replace("www.", '')
+    return url.split('.')[0]
+}

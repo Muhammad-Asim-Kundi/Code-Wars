915196613b22abddd821d88f8bfe459dbc722346
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-03-03 17:19:36 +0500

    return the total num of sheeps

---
+++ war1/js/sheepCount.js
@@ -0,0 +1,26 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+/** Code */
+
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    var num = 0;
+    
+    for(var i = 0; i < arrayOfSheep.length; i++)
+      if(arrayOfSheep[i] == true)
+        num++;
+        
+    return num;
+}

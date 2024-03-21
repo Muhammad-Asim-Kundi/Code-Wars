c057b6021aaf01c0c047d452415e0ccd4a99ab6f
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-03-21 20:43:16 +0500

    Short code for Coupun validation

--- war1/js/coupunCode.js
+++ war1/js/coupunCode.js
@@ -22,3 +22,8 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
     }
 }
   
+/** short code */
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+}

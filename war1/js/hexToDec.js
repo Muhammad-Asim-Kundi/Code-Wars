f7d760c4fb38dd8e0b4e2d1d9ca6daf969684135
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-03-26 10:56:21 +0500

    return the hex value in decimal

---
+++ war1/js/hexToDec.js
@@ -0,0 +1,10 @@
+// //task
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+/** Code */
+
+function hexToDec(hexString){
+    //your code here
+    return parseInt(hexString, 16)
+}

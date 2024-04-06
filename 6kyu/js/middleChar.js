a633b4a5f705f15e8c4ca19101520ddc16e38a3f
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-04-06 22:50:45 +0500

    Get middle char of String

--- 6kyu/js/middleChar.js
+++ 6kyu/js/middleChar.js
@@ -37,3 +37,19 @@ function getMiddle(s){
     // Extract the substring based on the determined position and length
     return s.substring(position, position + length);
 }
+
+/** Practicing again */
+
+function MiddleChar(str){
+  let position;
+  let length; 
+  if(str.length % 2 == 1){
+    position = str.length/2;
+    length = 1;
+  }else{
+    position = str.length/2-1;
+    length = 2
+  }
+
+  return str.substring(position,position+length)
+}

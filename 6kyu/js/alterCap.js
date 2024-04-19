f7301d211407bdcf4cf43ca986da078af03f7611
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-04-19 21:53:34 +0500

    Capitalize letter with even indexs

--- 6kyu/js/alterCap.js
+++ 6kyu/js/alterCap.js
@@ -24,3 +24,22 @@ function capitalize(s){
   
     return [even, odd];
 }
+
+/** Practice */
+
+function cap(s){
+    let odd = '';
+    let even = '';
+  
+    for(let i = 0; i < s.length; i++){
+        if(i % 2 === 0){
+            even += s[i].toUpperCase();
+            odd += s[i];
+        } else {
+            odd += s[i].toUpperCase();
+            even += s[i];
+        }
+    }
+  
+    return [even, odd];
+}

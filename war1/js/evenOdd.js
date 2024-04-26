b819783f360be7a0877ed516f010603958b53a82
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-04-26 22:37:54 +0500

    Even or Odd

---
+++ war1/js/evenOdd.js
@@ -0,0 +1,7 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+/** Code */
+
+function evenOrOdd(number) {
+    return number%2 == 0 ? 'Even' : 'Odd'
+}

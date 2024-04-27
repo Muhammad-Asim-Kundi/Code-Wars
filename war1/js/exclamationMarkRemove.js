8f45d4d23549458771ca3a52e3406638478736a4
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-04-27 20:06:51 +0500

    Remove last element

--- war1/js/exclamationMarkRemove.js
+++ war1/js/exclamationMarkRemove.js
@@ -14,5 +14,15 @@ function remove (string) {
     //string.slice(0, -1) returns a substring of the original string starting from the 
     //first character and ending at the character just before the last one
      return string.charAt(string.length - 1) === '!'? string.slice(0, -1) : string
-          
+         
+     
+
+}
+
+// Practice
+
+function removeExclamtion(string){
+    //string.slice(0, -1) returns a substring of the original string starting from the 
+    //first character and ending at the character just before the last one
+    return string.charAt(string.length-1) === '!' ? string.slice(0,-1) : string
 }

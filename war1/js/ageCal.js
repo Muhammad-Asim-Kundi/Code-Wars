987609538b1ec0468e56569a60e8fd639165e724
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-05-08 13:25:38 +0500

    Age calculation

--- war1/js/ageCal.js
+++ war1/js/ageCal.js
@@ -32,3 +32,10 @@ function  calculateAge(s,e) {
     
 }
   
+/** another short method */
+function  calAge(m, n) {
+	if(m == n) return 'You were born this very year!';
+  var year = Math.abs(m-n) == 1 ? 'year' : 'years';
+  if(m < n) return "You are "+(n-m)+' '+year+' old.';
+  if(m > n) return "You will be born in "+(-n+m)+' '+year+'.';
+}

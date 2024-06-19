6b93a26b9a36ddc3cd02189ee593fa330b4467e0
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-06-19 23:49:48 +0500

    Check if the array is sorted in ascending or descending or not.f

--- 6kyu/js/sortOrderCheck.js
+++ 6kyu/js/sortOrderCheck.js
@@ -39,3 +39,34 @@ function isSortedAndHow(array) {
       return "no";
     }
 }
+
+/** Practice */
+
+function isSortedHow(array){
+  
+    let isAscending = true
+    let isDescending = true
+  
+    // Iterate through the array, starting from the second element
+    for (let i = 1; i < array.length; i++) {
+      // If the current element is greater than the previous one, it means the array is not sorted in descending order
+      if (array[i] > array[i - 1]) {
+        isDescending = false;
+      }
+      // If the current element is less than the previous one, it means the array is not sorted in ascending order
+      else if (array[i] < array[i - 1]) {
+        isAscending = false;
+      }
+    }
+  
+    // Check the flags and return the appropriate result
+    if (isAscending) {
+      return "yes, ascending";
+    }
+    else if (isDescending) {
+      return "yes, descending";
+    }
+    else {
+      return "no";
+    }
+}

1d47c1066117039333f8b471d16e59dec7b583ed
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-06-20 23:25:30 +0500

    time needed in tortoise race for one tortoise to cross another.

--- 6kyu/js/race.js
+++ 6kyu/js/race.js
@@ -43,3 +43,9 @@ function tortoRace(v1, v2, g){
     let time=g/(v2-v1);
     return v2>v1 ? [Math.floor(time),Math.floor(time*60%60),Math.floor(time*3600%60)] : null;
 }
+
+/** Practice */
+function toRace(v1, v2, g){
+    let time=g/(v2-v1);
+    return v2>v1 ? [Math.floor(time),Math.floor(time*60%60),Math.floor(time*3600%60)] : null;
+}
